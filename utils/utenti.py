7ac8443fb7c8bb9de502e032e8e71811d1007b96
Martino <32127113+martinodn@users.noreply.github.com>
2025-08-06 21:32:12 +0200

    Update utenti.py

--- utils/utenti.py
+++ utils/utenti.py
@@ -8,7 +8,4 @@ def login(username, password):
     st.write("ciao")
     st.write(st.secrets["password"])
     st.write(username, password)
-    if (username not in st.secrets['password']):
-        st.warning("Specificare un nome utente valido.")
-    else:
-        return st.secrets['password'][username]==hash_password(password)
+    return st.secrets['password'][username]==hash_password(password)
