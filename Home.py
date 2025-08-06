2c3c80e222ff6bff79f5d44f624392a00f57c1fb
Martino <32127113+martinodn@users.noreply.github.com>
2025-08-06 22:18:39 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -13,16 +13,17 @@ if "logged_in" not in st.session_state:
 if not st.session_state.logged_in:
     username = st.sidebar.text_input("Username")
     password = st.sidebar.text_input("Password", type="password")
-    st.write(username, password)
-    if username in st.secrets["password"]:
-        st.write("yes")
-    if st.sidebar.button("Login"):
-        if login(username, password):
-            st.session_state.logged_in = True
-            st.session_state.username = username
-            st.rerun()
-        else:
-            st.sidebar.error("Credenziali errate")
+
+    if username not in st.secrets["password"]:
+        st.warning("Nome utente non esistente")
+    else:
+        if st.sidebar.button("Login"):
+            if login(username, password):
+                st.session_state.logged_in = True
+                st.session_state.username = username
+                st.rerun()
+            else:
+                st.sidebar.error("Credenziali errate")
 else:
     st.sidebar.success("Loggato come: {}".format(st.session_state.username))
     if st.sidebar.button("Logout"):
