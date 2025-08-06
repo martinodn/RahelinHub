5ba1689a2c7fa46379ad45177efe0e89b6dfbcd9
Martino <32127113+martinodn@users.noreply.github.com>
2025-08-06 21:33:58 +0200

    Update utenti.py

--- utils/utenti.py
+++ utils/utenti.py
@@ -5,7 +5,6 @@ def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 def login(username, password):
-    st.write("ciao")
-    st.write(st.secrets["password"])
+    
     st.write(username, password)
     return st.secrets['password'][username]==hash_password(password)
