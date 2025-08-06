24114d472aeb13bce98d4147789cf4c8b4ede454
Martino <32127113+martinodn@users.noreply.github.com>
2025-08-06 19:07:43 +0200

    Update utenti.py

--- utils/utenti.py
+++ utils/utenti.py
@@ -5,5 +5,7 @@ def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
 def login(username, password):
-    st.write(username, password)
-    return st.secrets['password'][username]==hash_password(password)
+    if (username is None) | (password is None):
+        st.warning("Inserire username e password.")
+    else:
+        return st.secrets['password'][username]==hash_password(password)
