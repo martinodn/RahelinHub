edb6309ad497fc08e13097efdceeda7db152bc50
Martino <32127113+martinodn@users.noreply.github.com>
2025-08-06 19:15:57 +0200

    Update utenti.py

--- utils/utenti.py
+++ utils/utenti.py
@@ -7,5 +7,7 @@ def hash_password(password):
 def login(username, password):
     if (username is None) | (password is None):
         st.warning("Inserire username e password.")
+    elif (username not in st.secrets['password']):
+        st.warning("Specificare un nome utente valido.")
     else:
         return st.secrets['password'][username]==hash_password(password)
